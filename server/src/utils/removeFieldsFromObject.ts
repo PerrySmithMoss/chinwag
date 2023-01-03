03afced44e44ab7f3c6250eca74c5be849137198
PerrySmithMoss <perrysmithmoss@gmail.com>
2023-01-03 10:35:10 +0000

    Changes for production

--- server/src/utils/removeFieldsFromObject.ts
+++ server/src/utils/removeFieldsFromObject.ts
@@ -1,24 +1,3 @@
-// import { User, Message, Post, Profile } from "@prisma/client";
-
-// type Object = User | Message | Post | Profile;
-
-// export const removeFieldsFromObject = (object: Object, fields: string[]) => {
-//   let newObj = { ...object };
-
-//   Object.keys(newObj).forEach((key) => {
-//     if (fields.includes(key)) {
-//       delete newObj[key as keyof object];
-//     }
-//   });
-
-//   return newObj;
-// };
-
-import {  Message, Profile } from "@prisma/client";
-import { User } from "../types/User";
-
-type Object = User | Message | Profile;
-
 export const removeFieldsFromObject = (object: any, fields: string[]) => {
   let newObj = { ...object };
 
